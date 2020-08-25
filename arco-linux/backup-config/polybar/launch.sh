7c742c54ebd8593f73d1f1bc3b4db073b24f7792
michael-antony-austin <aatralmike@gmail.com>
2020-08-25 21:06:24 +0530

    regular update

--- arco-linux/backup-config/polybar/launch.sh
+++ arco-linux/backup-config/polybar/launch.sh
@@ -26,14 +26,14 @@ case $desktop in
     else
     polybar --reload mainbar-i3 -c ~/.config/polybar/config &
     fi
-    # second polybar at bottom
-    # if type "xrandr" > /dev/null; then
-    #   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    #     MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
-    #   done
-    # else
-    # polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
-    # fi
+     second polybar at bottom
+     if type "xrandr" > /dev/null; then
+       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+         MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
+       done
+     else
+     polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
+     fi
     ;;
 
     openbox|/usr/share/xsessions/openbox)
