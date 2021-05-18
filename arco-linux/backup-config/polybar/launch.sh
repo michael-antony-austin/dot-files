8ca085b041e4ff00c9c66019aaf049d432a97f25
michael-antony-austin <aatralmike@gmail.com>
2021-05-18 21:02:05 +0530

    regular update

--- arco-linux/backup-config/polybar/launch.sh
+++ arco-linux/backup-config/polybar/launch.sh
@@ -19,6 +19,13 @@ count=$(xrandr --query | grep " connected" | cut -d" " -f1 | wc -l)
 case $desktop in
 
     i3|/usr/share/xsessions/i3)
+     if type "xrandr" > /dev/null; then
+       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+         MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
+       done
+     else
+     polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
+     fi
     if type "xrandr" > /dev/null; then
       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         MONITOR=$m polybar --reload mainbar-i3 -c ~/.config/polybar/config &
@@ -27,13 +34,6 @@ case $desktop in
     polybar --reload mainbar-i3 -c ~/.config/polybar/config &
     fi
      second polybar at bottom
-     if type "xrandr" > /dev/null; then
-       for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-         MONITOR=$m polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
-       done
-     else
-     polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
-     fi
     ;;
 
     openbox|/usr/share/xsessions/openbox)
